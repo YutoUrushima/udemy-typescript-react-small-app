03619619c38d462f42548e01499384fd168ff6bf
yuto urushima <yuto259233@gmail.com>
2021-06-29 22:14:18 +0900

    13. タスク作成機能

---
+++ src/TaskItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import firebase from "firebase/app";
+import { ListItem, TextField, Grid } from "@material-ui/core";
+import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+
+interface PROPS {
+  id: string;
+  title: string;
+}
+
+const TaskItem: React.FC<PROPS> = (props) => {
+  return <div></div>;
+};
+
+export default TaskItem;
